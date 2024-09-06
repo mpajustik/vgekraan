0d253a2093ef2aec79761b31627d1a319b3b355a
Merlis Pajustik <merlis.pajustik@gmail.com>
2024-09-06 22:12:27 +0300

    removed flask

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+#
+# if __name__ == '__main__':
+#     app.run(debug=False, port=5000)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+print("Hello World!")
