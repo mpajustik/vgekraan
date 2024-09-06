8e3374287a90f288ed00b983fb24ae31855629c1
Merlis Pajustik <merlis.pajustik@gmail.com>
2024-09-06 22:18:00 +0300

    vastseliina vg website reader

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-# from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-#
-# if __name__ == '__main__':
-#     app.run(debug=False, port=5000)
+import requests
+from bs4 import BeautifulSoup
 
-print("Hello World!")
+# URL veebilehelt
+url = 'https://www.vastseliina.edu.ee/'  # Asenda see soovitud veebilehe URL-iga
+
+# Tee HTTP päring
+response = requests.get(url)
+
+# Kontrolli, et päring õnnestus
+if response.status_code == 200:
+    # Parseti HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Leia kõik elemendid, mille class on "well-lg event-box"
+    event_boxes = soup.find_all(class_='well-lg event-box')
+    # print(event_boxes)
+
+    # Prindi välja iga "event-box" tekst
+    for box in event_boxes:
+        print(box.get_text(separator='\n', strip=True))
+else:
+    print(f'Veebilehe laadimine ebaõnnestus, statusi kood: {response.status_code}')
